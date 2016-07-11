f7aca7a5223403d87aa4414282252be1f39046fe
andersonlemos <anderson.lemos@dmorj.com.br>
2016-07-11 17:52:21 -0300

    update

--- www/js/services.js
+++ www/js/services.js
@@ -1,11 +1,23 @@
 angular.module('starter')
     .service('ProdutosService',function($http,$q){
       var url = 'http://cozinhapp.sergiolopes.org/produtos';
-      return {
+
+      var promisse= $http.get(url).then(function(response){
+          var json= JSON.stringify(response.data);
+          localStorage.setItem('cache',json);
+          return response.data;
+      });
+
+      var cache = localStorage.getItem('cache');
+
+      if(cache!=null){
+          promisse = $q(function(resolve,reject){
+          resolve(JSON.parse(cache));
+        });
+      }
+      return{
         lista: function(){
-          return $http.get(url).then(function(response){
-            return response.data;
-          });
+          return promisse;
         }
       };
-    });
+});
